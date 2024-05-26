d10a358a7d20b893e96771e54fbc07b3c026d0ec
alariojo99 <alejandrolario99@gmail.com>
2024-05-26 23:06:56 +0200

    komplet

--- src/data.js
+++ src/data.js
@@ -23,7 +23,7 @@ export default [
     {
         name: "Noma Raji",
         glb: "raji.glb",
-        current_material_principal: "material1",
+        current_material_principal: "material2",
         material_principal: {
             options: 3, 
             names: ["material1", "material2", "material3"],
@@ -45,13 +45,14 @@ export default [
     {
         name: "Nukak Klassik",
         glb: "klassik.glb",
-        current_material_principal: "material1",
+        current_material_principal: "material3",
         material_principal: {
             options: 3, 
             names: ["material1", "material2", "material3"],
             material1: "materialKlassik1",
             material2: "materialKlassik2",
             material3: "materialKlassik3",
+            
         },
         descripcion_principal: "Noma Nukak Klassik puede acoger de 4 a 6 personas en un diseño moderno y gran zona de cocina. Nukak Klassik es una Tiny House construida sobre un remolque, de 7,2 de largo y entre 2,48 y 2,5 metros de ancho, que se ajusta a las regulaciones Europeas (importante tener en cuenta cada país). La estructura de la Tiny House, incluido el remolque, tiene entre 3,8 y 4 metros de altura. <br> Es un modelo perfecto para los que les gusta cocinar y quieren dos espacios con intimidad. La habitación doble principal sirve de dormitorio, y la otra puede tener diversas utilidades como habitación individual, doble, con dos camas, espacio de estar, rincón de chillout y yoga o simplemente un espacio de almacenaje extra.",
         descripcion_secundaria: "<b>Klassik</b> es una tiny house para 2-4 personas. </br></br> <u>Incluye</u>: </br> <ul style='list-style:none;'> <li>Cama doble</li> <li>Sofá cama</li> <li>Altillo doble</li>  <li>Almacenamiento bajo las escaleras</li> <li>Mesa y silla abatible </li> <li>Armario de cuerpo entero</li> </ul>",
@@ -62,7 +63,30 @@ export default [
             longitud: 4,
             precio: "52.300€" 
         }
-    }   
+    },
+    
+    {
+        name: "Nukak Komplet",
+        glb: "komplet.glb",
+        current_material_principal: "material1",
+        material_principal: {
+            options: 3, 
+            names: ["material1", "material2", "material3", "material4"],
+            material1: "materialKomplet1",
+            material2: "materialKomplet2",
+            material3: "materialKomplet3",
+            material4: "materialKomplet4",
+        },
+        descripcion_principal: "Noma Nukak Komplet puede acoger de 4 a 6 personas en un diseño moderno y gran zona de cocina. Nukak Komplet es una Tiny House construida sobre un remolque, de 7,2 de largo y entre 2,48 y 2,5 metros de ancho, que se ajusta a las regulaciones Europeas (importante tener en cuenta cada país). La estructura de la Tiny House, incluido el remolque, tiene entre 3,8 y 4 metros de altura. <br> Es un modelo perfecto para los que les gusta cocinar y quieren dos espacios con intimidad. La habitación doble principal sirve de dormitorio, y la otra puede tener diversas utilidades como habitación individual, doble, con dos camas, espacio de estar, rincón de chillout y yoga o simplemente un espacio de almacenaje extra.",
+        descripcion_secundaria: "<b>Komplet</b> es una tiny house para 2-4 personas. </br></br> <u>Incluye</u>: </br> <ul style='list-style:none;'> <li>Cama doble</li> <li>Sofá cama</li> <li>Altillo doble</li>  <li>Almacenamiento bajo las escaleras</li> <li>Mesa y silla abatible </li> <li>Armario de cuerpo entero</li> </ul>",
+        caracteristicas: {
+            superficie: 17,
+            altura: 4,
+            anchura: 4,
+            longitud: 4,
+            precio: "52.300€" 
+        }
+    }  
     
 
 
